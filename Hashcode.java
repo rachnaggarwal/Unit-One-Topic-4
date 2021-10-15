d2e92c9d6b0aadf03b5e6932bec4a38e1b3aaf53
Rachna <aggrachna20@gmail.com>
2021-10-15 12:06:38 +0530

    Add files via upload

---
+++ Hashcode.java
@@ -0,0 +1,8 @@
+package Demo;
+import java.io.*;
+public class Hashcode {
+	   public static void main(String args[]) {
+		      String Str = new String("This program is for checking working of Hascode method");
+		      System.out.println("Hashcode for Str :" + Str.hashCode() );
+		   }
+}
